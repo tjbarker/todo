6b8b8151c0a4babf32b02e72930bfc013a55f21b
Thomas Barker <thomas.john.barker@gmail.com>
2015-08-18 21:47:28 +1000

    add length and presence validation to title

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe 'Creating todo lists' do
+  it 'redirects to the todo list index page on success' do
+    visit '/todo_lists'
+    click_link 'New Todo list'
+
+    expect(page).to have_content('New Todo List')
+
+    fill_in 'Title', with: 'this is the title'
+    fill_in 'Description', with: 'this is the desciption'
+    click_button 'Create Todo list'
+
+    expect(page).to have_content('this is the title')
+  end
+
+  it 'displays error when todo list has no title' do
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+
+    expect(page).to have_content('New Todo List')
+
+    fill_in 'Title', with: ''
+    fill_in 'Description', with: 'this is the desciption'
+    click_button 'Create Todo list'
+
+    expect(TodoList.count).to eq(0)
+    expect(page).to have_content('error')
+
+    visit '/todo_lists'
+    expect(page).to_not have_content('this is the title')
+  end
+
+  it 'displays error when todo list title has less than 3 characters' do
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+
+    expect(page).to have_content('New Todo List')
+
+    fill_in 'Title', with: '12'
+    fill_in 'Description', with: 'this is the desciption'
+    click_button 'Create Todo list'
+
+    expect(TodoList.count).to eq(0)
+    expect(page).to have_content('error')
+
+    visit '/todo_lists'
+    expect(page).to_not have_content('this is the title')
+  end
+end
