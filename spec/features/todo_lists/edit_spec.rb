8a51cda0976478583ebd89124eee53a51965ce51
Thomas Barker <thomas.john.barker@gmail.com>
2015-08-18 22:50:59 +1000

    edited edit_spec.rb to test length and presence of titles and description

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe 'Editing todo lists' do
   let!(:todo_list) { todo_list = TodoList.create(title: '0Title', description: '0Description') }
+
   def update_todo_list(options={})
     options[:title] ||= 'Title_Edited'
     options[:description] ||='Description_Edited'
-    todo_list = options[:todo_list]
+    todo_list = options[:todo_list] ||= todo_list
 
     visit '/todo_lists'
 
@@ -21,11 +22,33 @@ describe 'Editing todo lists' do
   end
 
   it 'updates a todo list successfully with correct information' do
-
     update_todo_list todo_list: todo_list
 
     expect(page).to have_content('Todo list was successfully updated')
     expect(todo_list.title).to eq('Title_Edited')
     expect(todo_list.description).to eq('Description_Edited')
+  end
+
+  it 'displays error with no title' do
+    update_todo_list todo_list: todo_list, title: ''
+    title = todo_list.title
+    todo_list.reload
+    expect(todo_list.title).to eq(title)
+    expect(page).to have_content('error')
+  end
+
+  it 'displays error with too short a title' do
+    update_todo_list todo_list: todo_list, title: '12'
+    expect(page).to have_content('error')
+  end
+
+  it 'displays error with no description' do
+    update_todo_list todo_list: todo_list, description: ''
+    expect(page).to have_content('error')
+  end
+
+  it 'displays error with too short a description' do
+    update_todo_list todo_list: todo_list, description: '1234'
+    expect(page).to have_content('error')
   end
 end
